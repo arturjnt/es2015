996c97cd7c4d72ca2ef4afbdd70e0c54ba3f5ed0
Artur Teixeira <arturjnt@gmail.com>
2016-02-06 15:44:42 +0100

    Rest params. Spread operator. Object initializer shorthand.

--- script-compiled.js
+++ script-compiled.js
@@ -31,3 +31,32 @@ let createPersonAndPrint = (name, age, country) => {
 createPersonAndPrint(undefined, undefined, undefined);
 createPersonAndPrint('André');
 createPersonAndPrint('Sr. Carreira', 32);
+
+// Rest params
+function iterateArray() {
+  for (var _len = arguments.length, iteratable = Array(_len), _key = 0; _key < _len; _key++) {
+    iteratable[_key] = arguments[_key];
+  }
+
+  for (let element in iteratable) {
+    console.log(iteratable[element]);
+  }
+}
+
+iterateArray('ES2015', 'is', 'awesome!');
+
+// Spread operator
+let x = 'first',
+    y = 'second',
+    z = 'third';
+let xyz = [x, y, z];
+
+function spreadParams(x, y, z) {
+  console.log(`x: ${ x } | y: ${ y } | z: ${ z }`);
+}
+
+spreadParams.apply(undefined, xyz);
+
+// Object initializer shorthand
+let shorty = { x, y, z };
+console.log(`x: ${ shorty.x } | y: ${ shorty.y } | z: ${ shorty.z }`);
--- script.js
+++ script.js
@@ -20,3 +20,26 @@ let createPersonAndPrint = (name, age, country) => {
 createPersonAndPrint(undefined, undefined, undefined);
 createPersonAndPrint('André');
 createPersonAndPrint('Sr. Carreira', 32);
+
+// Rest params
+function iterateArray(...iteratable) {
+  for (let element in iteratable) {
+    console.log(iteratable[element]);
+  }
+}
+
+iterateArray('ES2015', 'is', 'awesome!');
+
+// Spread operator
+let x = 'first', y = 'second', z = 'third';
+let xyz = [x, y, z];
+
+function spreadParams(x, y, z) {
+  console.log(`x: ${x} | y: ${y} | z: ${z}`);
+}
+
+spreadParams(...xyz);
+
+// Object initializer shorthand
+let shorty = {x, y, z};
+console.log(`x: ${shorty.x} | y: ${shorty.y} | z: ${shorty.z}`);
