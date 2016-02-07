23bb933533282c89b24b1d0ac5d88652dd285907
Artur Teixeira <arturjnt@gmail.com>
2016-02-07 16:22:09 +0100

    Classes

--- script-compiled.js
+++ script-compiled.js
@@ -142,3 +142,41 @@ for (let tag of tags) {
 }
 
 console.log(tags.has('Web'));
+
+// Classes
+class Advertisement {
+
+  constructor(title, link) {
+    this.title = title;
+    this.link = link;
+  }
+
+  _buildContent() {
+    return `<h1>${ this.title }</h1>
+      <a href="${ this.link }">${ this._linkText() }</a>`;
+  }
+
+  _linkText() {
+    return "Click Here";
+  }
+
+  render() {
+    return this._buildContent();
+  }
+}
+
+// Here's an example of how this class is used:
+/*
+  let ad = new Advertisement("CodeSchool", "http://codeschool.com");
+  _appendToPage( targetElement, ad.render() );
+*/
+
+class SidebarAdvertisement extends Advertisement {
+  constructor(title, link) {
+    super(title, link);
+  }
+
+  _linkText() {
+    return 'Sign up now!';
+  }
+}
